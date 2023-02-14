a628233f0651ab0a62a88abc8953c9eb519cad06
Anton Smirnov <sandfox@sandfox.me>
2023-02-14 08:10:09 +0200

    NameHelper might be needed outside

--- src/Helpers/NameHelper.php
+++ src/Helpers/NameHelper.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Arokettu\ArithmeticParser\Helpers;
 
-/**
- * @internal
- */
 final class NameHelper
 {
     public static function normalizeVar(string $name): string
