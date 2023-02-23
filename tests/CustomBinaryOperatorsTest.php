617505e03026492bfa584146583f6a3c02572c2b
Anton Smirnov <sandfox@sandfox.me>
2023-02-23 13:12:01 +0200

    Test custom bin operators

---
+++ tests/CustomBinaryOperatorsTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arokettu\ArithmeticParser\Tests;
+
+use Arokettu\ArithmeticParser\Calculator;
+use Arokettu\ArithmeticParser\Config;
+use PHPUnit\Framework\TestCase;
+
+class CustomBinaryOperatorsTest extends TestCase
+{
+    public function testLeftAssociative(): void
+    {
+        $config = Config::default()->addOperator(
+            new Config\BinaryOperator('^', pow(...), Config\BinaryOperator::PRIORITY_POW, Config\Association::LEFT)
+        );
+
+        $eval = Calculator::evaluate('2 ^ 3 ^ 4', $config);
+
+        self::assertEquals((2 ** 3) ** 4, $eval);
+    }
+
+    public function testRightAssociative(): void
+    {
+        $config = Config::default()->addOperator(
+            new Config\BinaryOperator('^', pow(...), Config\BinaryOperator::PRIORITY_POW, Config\Association::RIGHT)
+        );
+
+        $eval = Calculator::evaluate('2 ^ 3 ^ 4', $config);
+
+        self::assertEquals(2 ** 3 ** 4, $eval);
+    }
+
+    public function testMulticharOperators(): void
+    {
+        $config = Config::default()->addOperator(
+            new Config\BinaryOperator('**', pow(...), Config\BinaryOperator::PRIORITY_POW, Config\Association::RIGHT)
+        );
+
+        // ** should take precedence over *
+        $eval = Calculator::evaluate('2 * 3 ** 4', $config);
+
+        self::assertEquals(2 * 3 ** 4, $eval);
+    }
+
+    public function testAlphaOperators(): void
+    {
+        $config = Config::default()->setOperators(
+            new Config\BinaryOperator('add', fn ($a, $b) => $a + $b, Config\BinaryOperator::PRIORITY_ADD),
+            new Config\BinaryOperator('mul', fn ($a, $b) => $a * $b, Config\BinaryOperator::PRIORITY_MUL),
+        );
+
+        // ** should take precedence over *
+        $eval = Calculator::evaluate('2 add 2 mul 2', $config);
+
+        self::assertEquals(2 + 2 * 2, $eval);
+    }
+}
