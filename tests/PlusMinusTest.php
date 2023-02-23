7b40e60cf89316304276c07f0f1791f2ce11af29
Anton Smirnov <sandfox@sandfox.me>
2023-02-23 14:27:31 +0200

    Postfix combination test

--- tests/PlusMinusTest.php
+++ tests/PlusMinusTest.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace Arokettu\ArithmeticParser\Tests;
 
 use Arokettu\ArithmeticParser\Calculator;
+use Arokettu\ArithmeticParser\Config;
+use Arokettu\ArithmeticParser\Config\UnaryOperator;
+use Arokettu\ArithmeticParser\Config\UnaryPos;
 use PHPUnit\Framework\TestCase;
 
 class PlusMinusTest extends TestCase
@@ -21,5 +24,19 @@ class PlusMinusTest extends TestCase
         self::assertEquals(5, Calculator::evaluate('+(2+3)'));
         self::assertEquals(-2, Calculator::evaluate('-2'));
         self::assertEquals(-5, Calculator::evaluate('-(2+3)'));
+    }
+
+    public function testUnaryCombination(): void
+    {
+        $config = Config::default()->addOperators(
+            new UnaryOperator('¿', fn ($a) => $a * 2, UnaryPos::PREFIX),
+            new UnaryOperator('¡', fn ($a) => $a + 2, UnaryPos::PREFIX),
+            new UnaryOperator('?', fn ($a) => $a * 2, UnaryPos::POSTFIX),
+            new UnaryOperator('!', fn ($a) => $a + 2, UnaryPos::POSTFIX),
+        );
+
+        // postfix
+        $val = Calculator::evaluate('-2?! + -2!?', $config);
+        self::assertEquals(-(2 * 2 + 2) + -((2 + 2) * 2), $val);
     }
 }
