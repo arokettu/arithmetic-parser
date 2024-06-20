e513982c77c567e26ea9791abddf1d97fceae358
Anton Smirnov <sandfox@sandfox.me>
2024-06-20 15:05:40 +0300

    Actually test the order

--- tests/UnaryOperatorsTest.php
+++ tests/UnaryOperatorsTest.php
@@ -15,39 +15,46 @@ class UnaryOperatorsTest extends TestCase
     public function testPrefixOrder(): void
     {
         $config = Config::default()->addOperators(
-            new UnaryOperator('¿', fn ($a) => $a * 2, UnaryPos::PREFIX),
-            new UnaryOperator('¡', fn ($a) => $a + 2, UnaryPos::PREFIX),
+            new UnaryOperator('¿', fn ($a) => $a * 3, UnaryPos::PREFIX),
+            new UnaryOperator('¡', fn ($a) => $a + 5, UnaryPos::PREFIX),
         );
 
         // right to left
         $calc = Calculator::evaluate('¡¿2', $config);
+        self::assertEquals(2 * 3 + 5, $calc);
 
-        self::assertEquals(2 + 2 * 2, $calc);
+        $calc = Calculator::evaluate('¿¡2', $config);
+        self::assertEquals((2 + 5) * 3, $calc);
     }
 
     public function testPostfixOrder(): void
     {
         $config = Config::default()->addOperators(
-            new UnaryOperator('?', fn ($a) => $a * 2, UnaryPos::POSTFIX),
-            new UnaryOperator('!', fn ($a) => $a + 2, UnaryPos::POSTFIX),
+            new UnaryOperator('?', fn ($a) => $a * 3, UnaryPos::POSTFIX),
+            new UnaryOperator('!', fn ($a) => $a + 5, UnaryPos::POSTFIX),
         );
 
         // right to left
         $calc = Calculator::evaluate('2?!', $config);
+        self::assertEquals(2 * 3 + 5, $calc);
 
-        self::assertEquals(2 + 2 * 2, $calc);
+        $calc = Calculator::evaluate('2!?', $config);
+        self::assertEquals((2 + 5) * 3, $calc);
     }
 
     public function testMixedOrder(): void
     {
         $config = Config::default()->addOperators(
-            new UnaryOperator('?', fn ($a) => $a * 2, UnaryPos::POSTFIX),
-            new UnaryOperator('¿', fn ($a) => $a + 2, UnaryPos::PREFIX),
+            new UnaryOperator('?', fn ($a) => $a * 3, UnaryPos::POSTFIX),
+            new UnaryOperator('¿', fn ($a) => $a + 5, UnaryPos::PREFIX),
         );
 
         // postfix first
         $calc = Calculator::evaluate('¿2?', $config);
+        self::assertEquals(2 * 3 + 5, $calc);
 
-        self::assertEquals(2 + 2 * 2, $calc);
+        // alter with brackets
+        $calc = Calculator::evaluate('(¿2)?', $config);
+        self::assertEquals((2 + 5) * 3, $calc);
     }
 }
