a4a2e3c3252274111890bc82d3f533c01229a258
Anton Smirnov <sandfox@sandfox.me>
2024-07-12 21:26:52 +0300

    Validate methods

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -8,6 +8,8 @@ use Arokettu\ArithmeticParser\Config;
 use Arokettu\ArithmeticParser\Exceptions\MissingFunctionArgumentsException;
 use Arokettu\ArithmeticParser\Exceptions\MissingFunctionsException;
 use Arokettu\ArithmeticParser\Exceptions\MissingVariablesException;
+use Arokettu\ArithmeticParser\Operation\FunctionCall;
+use Arokettu\ArithmeticParser\Operation\Variable;
 use Arokettu\ArithmeticParser\Parser;
 use Arokettu\ArithmeticParser\Validator;
 use PHPUnit\Framework\TestCase;
@@ -16,26 +18,95 @@ class ValidatorTest extends TestCase
 {
     public function testValidate(): void
     {
-        // todo
-        $this->markTestSkipped();
+        $parsedValid = (new Parser())->parse('a + b * c + log(123) + abs(-2)');
+        // try to collect all problems
+        $parsedInvalid = (new Parser())->parse('a + b + c() + d(123) + if(1) + log()');
+        $config = Config::default();
+
+        $warnings = [
+            new Validator\MissingVariablesWarning([
+                'A' => new Variable('a'),
+                'B' => new Variable('b'),
+            ]),
+            new Validator\MissingFunctionsWarning([
+                'C' => new FunctionCall('c', 0),
+                'D' => new FunctionCall('d', 1),
+            ]),
+            new Validator\MissingFunctionArgumentsWarning(
+                $config->getFunctions()['IF'],
+                new FunctionCall('if', 1),
+            ),
+            new Validator\MissingFunctionArgumentsWarning(
+                $config->getFunctions()['LOG'],
+                new FunctionCall('log', 0),
+            ),
+        ];
+
+        self::assertEquals([], Validator::validate($parsedValid, $config, ['a', 'b', 'c']));
+        self::assertEquals($warnings, Validator::validate($parsedInvalid, $config, []));
     }
 
     public function testIsValid(): void
     {
-        // todo
-        $this->markTestSkipped();
+        $parsedValid = (new Parser())->parse('a + b * c + log(123) + abs(-2)');
+        // try to collect all problems
+        $parsedInvalid = (new Parser())->parse('a + b + c() + d(123) + if(1) + log()');
+        $config = Config::default();
+
+        self::assertTrue(Validator::isValid($parsedValid, $config, ['a', 'b', 'c']));
+        self::assertFalse(Validator::isValid($parsedInvalid, $config, []));
+    }
+
+    public function testIsValidWithWarnings(): void
+    {
+        $parsedValid = (new Parser())->parse('a + b * c + log(123) + abs(-2)');
+        // try to collect all problems
+        $parsedInvalid = (new Parser())->parse('a + b + c() + d(123) + if(1) + log()');
+        $config = Config::default();
+
+        $warnings = [
+            new Validator\MissingVariablesWarning([
+                'A' => new Variable('a'),
+                'B' => new Variable('b'),
+            ]),
+            new Validator\MissingFunctionsWarning([
+                'C' => new FunctionCall('c', 0),
+                'D' => new FunctionCall('d', 1),
+            ]),
+            new Validator\MissingFunctionArgumentsWarning(
+                $config->getFunctions()['IF'],
+                new FunctionCall('if', 1),
+            ),
+            new Validator\MissingFunctionArgumentsWarning(
+                $config->getFunctions()['LOG'],
+                new FunctionCall('log', 0),
+            ),
+        ];
+
+        self::assertTrue(Validator::isValid($parsedValid, $config, ['a', 'b', 'c'], $warningsValid));
+        self::assertEquals([], $warningsValid);
+        self::assertFalse(Validator::isValid($parsedInvalid, $config, [], $warningsInvalid));
+        self::assertEquals($warnings, $warningsInvalid);
     }
 
     public function testAssertValidValid(): void
     {
-        // todo
-        $this->markTestSkipped();
+        $parsedValid = (new Parser())->parse('a + b * c + log(123) + abs(-2)');
+
+        Validator::assertValid($parsedValid, Config::default(), ['a', 'b', 'c']);
+
+        $this->expectNotToPerformAssertions(); // mark success here
     }
 
     public function testAssertValidInvalid(): void
     {
-        // todo
-        $this->markTestSkipped();
+        // try to collect all problems
+        $parsedInvalid = (new Parser())->parse('a + b + c() + d(123) + if(1) + log()');
+
+        $this->expectException(MissingVariablesException::class); // the first warning will throw
+        $this->expectExceptionMessage('Missing variables: a, b');
+
+        Validator::assertValid($parsedInvalid, Config::default(), []);
     }
 
     public function testMissingVariables(): void
